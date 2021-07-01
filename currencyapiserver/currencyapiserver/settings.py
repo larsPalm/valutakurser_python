d9e9019b148154071c2192ba97c825f2b691b40e
Lars Palm <larspalm@Larss-MacBook-Pro.local>
2021-07-01 10:41:21 +0200

    all code for currencyapiserver

--- currencyapiserver/currencyapiserver/settings.py
+++ currencyapiserver/currencyapiserver/settings.py
@@ -20,7 +20,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'k_@xv&2$(mu3wjx5e@=r+&)=(5%k+6g@b9)69quzny%0xj6qii'
 SECRET_KEY = os.getenv('currency_server_secret_key')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
