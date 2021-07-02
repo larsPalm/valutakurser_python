e096ef95ab1117b6011959be4d59bd9ebe048499
Lars Palm <larspalm@Larss-MacBook-Pro.local>
2021-07-02 17:30:38 +0200

    oppdatert kode fsom støtter visning av grafer

--- api_server/end_point/views.py
+++ api_server/end_point/views.py
@@ -10,6 +10,12 @@ import pandas as pd
 from pandas import DataFrame
 import json
 from django.conf import settings
+import datetime
+from random import randint
+import matplotlib.pyplot as plt
+import io, base64
+from plotly.offline import plot
+import plotly.graph_objects as go
 
 
 def index(request):
@@ -65,7 +71,11 @@ def get_currency(request):
     base_curs = []
     for elm in sql_response:
         base_curs.append(elm['cur_name'])
-    return HttpResponse(json.dumps({'info':['list of our supported currencies',
+    return HttpResponse(json.dumps({'created by':{'name':'Lars Palm',
+                                                  'Gtihub':'https://github.com/larsPalm',
+                                                  'Country':'Kolbotn,Norway',
+                                                  'When':'Summer 2021'},
+                                    'info':['list of our supported currencies',
                                             'made just for fun, not professional'],
                                     'url for data':'http://127.0.0.1:8080/get_info/',
                                     'supported currencies': base_curs}))
@@ -109,5 +119,30 @@ def convert(request):
 
 
 def compare(request):
+    sql_response = Currency_value.objects.values('cur_name').distinct()
+    base_curs = []
+    for elm in sql_response:
+        base_curs.append(elm['cur_name'])
     #return HttpResponse('Fuck')
-    return render(request, 'compare.html')
+    if request.POST:
+        print(request.POST)
+        to_cur = request.POST['to_cur']
+        from_cur = request.POST['from_cur']
+        if to_cur not in base_curs or from_cur not in base_curs:
+            return render(request, 'compare.html', {'bases': base_curs,'msg':'invalid input'})
+        else:
+            date_dict = Currency_value.objects.values('dato').distinct()
+            datoer = [elm['dato'] for elm in date_dict]
+            cur1_values = [Currency_value.objects.get(dato=date, cur_name=from_cur).value for date in datoer]
+            cur2_values = [Currency_value.objects.get(dato=date, cur_name=to_cur).value for date in datoer]
+            graph_value = [round(v1 / v2, 4) for v1, v2 in zip(cur1_values, cur2_values)]
+            x_values = [datetime.datetime.strptime(d, "%Y-%m-%d").date() for d in datoer]
+            graphs = []
+            fig = go.Scatter(x=x_values,y=graph_value,mode='lines',fill='tozeroy')
+            graphs.append(fig)
+            layout = go.Layout(title='{} vs {}'.format(from_cur, to_cur),
+                               yaxis=dict(range=[min(graph_value), max(graph_value)]))
+            plot_div = plot({'data': graphs, 'layout': layout},
+                            output_type='div')
+            return render(request, 'compare.html', {'bases': base_curs,'plot_div': plot_div})
+    return render(request, 'compare.html', {'bases': base_curs})
